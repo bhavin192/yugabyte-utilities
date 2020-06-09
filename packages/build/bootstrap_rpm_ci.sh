73da3ae45ea6f37b8e5b5af63c2ccb5e5da33065
Bhavin Gandhi <bhavin7392@gmail.com>
2020-06-09 11:33:43 +0530

    Update the packages version to 2.2
    - Pinning the ffi version for CentOS 7 in CI as the latest version
      requires Ruby 2.3, which isn't directly available.
    
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

--- packages/build/bootstrap_rpm_ci.sh
+++ packages/build/bootstrap_rpm_ci.sh
@@ -8,5 +8,8 @@ yum install rubygems ruby-devel gcc redhat-rpm-config make rpm-build -y
 echo 'export PATH="$PATH:${HOME}/bin"' >> "${HOME}/.bashrc"
 source "${HOME}/.bashrc"
 
+# Pinning this as CentOS 7 doesn't have Ruby 2.3 and ffi > 1.12.2
+# requires that.
+gem install ffi --version "1.12.2"
 gem install --no-document fpm
 fpm --version
